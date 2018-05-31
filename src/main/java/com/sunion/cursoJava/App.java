ccd86f1138a0e10a836083a533f2a85a51aa8bd7
miguelhdezblanco <39797636+miguelhdezblanco@users.noreply.github.com>
2018-05-31 14:10:18 +0200

    primer commit
    subida git

---
+++ src/main/java/com/sunion/cursoJava/App.java
@@ -0,0 +1,13 @@
+package com.sunion.cursoJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
